861fac3cc5c20134cebedc9a60851b4859e7a0ba
Phoenix Rigo <phoenix.rigo@gmail.com>
2018-11-26 16:56:19 +0100

    hfhfx

--- main/js/main.js
+++ main/js/main.js
@@ -53,14 +53,20 @@
         flag = 0;
         document.getElementById('main').style = "transform: translate(" + (tris_W/2) + "px, 0px);";
         document.getElementById('main').style.width = ((tris_W * 2) + 200);
+        document.getElementsByClassName('container')[0].style.opacity = 1;
 
-        document.getElementById('C_contact').style = "transition: opacity ease 0.3s; transition-delay: 0.3s; opacity: 1; position: absolute; transform: translate(" + (tris_W/2)*-1 + "px, " + (tris_H)*-1 + "px);";
+        document.getElementsByClassName('line')[0].style = "stroke-dashoffset: 0;";
+        document.getElementsByClassName('txt')[0].style.height = "800px";
+        //document.getElementById('C_contact').style = "transition: opacity ease 0.3s; transition-delay: 0.3s; opacity: 1; position: absolute; transform: translate(" + (tris_W/2)*-1 + "px, " + (tris_H)*-1 + "px);";
       }
       else {
         flag = 1;
         document.getElementById('main').style = "transform: translate(0px, 0px);";
+        document.getElementsByClassName('container')[0].style.opacity = 0;
+        //document.getElementById('C_contact').style = "opacity: 0;"
 
-        document.getElementById('C_contact').style = "opacity: 0;"
+        document.getElementsByClassName('line')[0].style = "stroke-dashoffset: -1000;";
+        document.getElementsByClassName('txt')[0].style.height = "0px";
       }
     }
 
@@ -77,70 +83,3 @@
         document.getElementById('main').style = "transform: translate(0px, 0px);";
       }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if (Habitude, Un fait, Qui ne s’arrête pas) {
-  function Present_Simple() {
-    if (Afirmatif) {
-      le verbe à l’infinitif
-      ex : “You eat apples everyday.”
-    }
-
-    if (Intérogatif) {
-      auxilière (Do(do || Does), be, have)
-      ex : “Do you eat apples everyday ?”
-    }
-
-    if (Négatif) {
-      auxilière (Do = don’t || Does = doesn’t || Have = Haven’t || Has = Hasen’t)
-      ex : “You don’t eat apples everyday.”
-    }
-  }
-}
